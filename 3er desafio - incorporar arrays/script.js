f6cba11e5661428a0a242361b5d5205fe00bfc64
pietrochtt <chiambrettopietro@gmail.com>
2022-06-29 21:53:05 -0300

    desafio3

--- 3er desafio - incorporar arrays/script.js
+++ 3er desafio - incorporar arrays/script.js
@@ -1,26 +1,10 @@
-// const productoCompras = []
-
-// let productoAgregar = prompt("Ingrese su producto para agregar a la lista").toLowerCase()
-
-
-// while(productoAgregar != "esc") {
-//     productoCompras.push(productoAgregar)
-//     console.table(productoCompras)
-//     productoAgregar = prompt("Ingrese otro producto para agregar a la lista").toLowerCase()
-// }
-
-
-
 const productoCompras = []
 
-let productoAgregar = prompt("Si desea agregar productos hagalo con el signo +, + producto, si desea quitar hagalo con el signo -, mas producto").toLowerCase()
-let indice = productoCompras.indexOf(productoEliminar)
+let productoAgregar = prompt("Ingrese su producto para agregar a la lista de compras").toLowerCase()
 
-if(productoAgregar != "esc" || productoAgregar != "-" + productoAgregar ) {
+
+while(productoAgregar != "esc") {
     productoCompras.push(productoAgregar)
     console.table(productoCompras)
     productoAgregar = prompt("Ingrese otro producto para agregar a la lista").toLowerCase()
-} else if (indice >= 0) {
-    productoCompras.splice(indice,1)
-    console.table(productoCompras)
 }
